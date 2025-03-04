8775ad0ab19dd334a86afef279227e0dd50d9af4
chiplus <shon910713@gamil.com>
2025-03-04 15:56:12 +0800

    test update

--- app/(tabs)/helper.tsx
+++ app/(tabs)/helper.tsx
@@ -6,6 +6,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+// import
 
 export default function TabTwoScreen() {
   return (
